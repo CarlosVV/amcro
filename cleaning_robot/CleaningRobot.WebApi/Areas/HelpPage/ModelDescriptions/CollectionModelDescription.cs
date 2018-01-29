0ee9cab807011c4c526c9463cbd8a2b2619694bb
Carlos Valderrama <cvalderramav@gmail.com>
2018-01-29 07:38:30 -0300

    Fixing Web API

--- cleaning_robot/CleaningRobot.WebApi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace CleaningRobot.WebApi.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
