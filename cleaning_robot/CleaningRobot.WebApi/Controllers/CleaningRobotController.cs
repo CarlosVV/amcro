a613381b35856ada59a0c514c049aad0db0faa50
Carlos Valderrama <cvalderramav@gmail.com>
2018-01-29 07:22:40 -0300

    Adding Web API Project to use the Library

---
+++ cleaning_robot/CleaningRobot.WebApi/Controllers/CleaningRobotController.cs
@@ -0,0 +1,24 @@
+﻿using CleaningRobot.Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CleaningRobot.WebApi.Controllers
+{
+    public class CleaningRobotController : ApiController
+    {
+        protected Robot robot = new Robot(new Battery());
+        public CleaningRobotController()
+        {
+
+        }
+        [Route("robot/clean")]
+        public IHttpActionResult Post(CleaningRequest request)
+        {
+            return Ok(robot.Run(request));
+        }
+    }
+}
