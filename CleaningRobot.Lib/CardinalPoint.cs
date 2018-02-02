7385ebf69b887b8673fd4e7571dd3c60150749fb
cvalderrama <cvalderrama@riafinancial.com>
2018-02-02 18:29:33 -0300

    Adding Encapsulation for Cardinal Points

---
+++ CleaningRobot.Lib/CardinalPoint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleaningRobot.Lib
+{
+    public class CardinalPoint
+    {
+        private string Name { get; set;}
+        private Facing Facing { get; set; }
+        public CardinalPoint(Facing facing)
+        {
+            Facing = facing;
+        }
+        public string ToString()
+        {
+            return Name;
+        }
+    }
+}
